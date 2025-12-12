c15211def154f3ff53b2e7a9c4df4031e5055f24
Isa890 <isaacbabalola06@gmail.com>
2025-12-12 10:17:57 -0800

    v.1.0

---
+++ main.js
@@ -0,0 +1,12 @@
+// let test1= prompt('How was your day metabolite?')
+// // console.log(test1);
+
+const toastTrigger = document.getElementById('liveToastBtn')
+const toastLiveExample = document.getElementById('liveToast')
+
+if (toastTrigger) {
+  const toastBootstrap = bootstrap.Toast.getOrCreateInstance(toastLiveExample)
+  toastTrigger.addEventListener('click', () => {
+    toastBootstrap.show()
+  })
+}
